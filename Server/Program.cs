ac1ed6f49c2fb5abb004fc62e23c68f8dae1e42a
Jon Hilton <hilton.jon@gmail.com>
2021-11-08 13:03:42 +0000

    Sorting out db extension

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=dashboard"));
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlite("Data Source=dashboard.sqlite"));
 
 builder.Services.AddScoped<DashboardQuery>();
 builder.Services.AddScoped<AddPanelCommandHandler>();
